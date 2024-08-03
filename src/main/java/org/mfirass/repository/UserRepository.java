33a5ffde020b866b22fb10915d21c2720e134fe1
Mohammed Firass <mohammed.firass@oracle.com>
2024-08-03 16:36:49 +0100

    initiate User and Book repositories.

---
+++ src/main/java/org/mfirass/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.mfirass.repository;
+
+import org.mfirass.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
