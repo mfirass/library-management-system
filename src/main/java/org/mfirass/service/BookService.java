842b6da318a80e371b75502f6e8ae51fde32d5d9
Mohammed Firass <mohammed.firass@oracle.com>
2024-08-03 16:41:13 +0100

    implement User and Book services.

--- src/main/java/org/mfirass/service/BookService.java
+++ src/main/java/org/mfirass/service/BookService.java
@@ -1,7 +1,22 @@
 package org.mfirass.service;
 
+import org.mfirass.model.Book;
+import org.mfirass.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
 }
--- src/main/java/org/mfirass/service/UserService.java
+++ src/main/java/org/mfirass/service/UserService.java
@@ -1,7 +1,22 @@
 package org.mfirass.service;
 
+import org.mfirass.model.User;
+import org.mfirass.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveBook(User user) {
+        return userRepository.save(user);
+    }
 }
