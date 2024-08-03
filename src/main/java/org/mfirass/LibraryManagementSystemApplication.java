335ca6bdec6fbeccee2ec95e9c72849797a1335a
Mohammed Firass <mohammed.firass@oracle.com>
2024-08-03 17:26:15 +0100

    set the main entry point to the app.

--- src/main/java/org/mfirass/LibraryManagementSystemApplication.java
+++ src/main/java/org/mfirass/LibraryManagementSystemApplication.java
@@ -1,7 +1,11 @@
 package org.mfirass;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LibraryManagementSystemApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(LibraryManagementSystemApplication.class, args);
     }
 }
